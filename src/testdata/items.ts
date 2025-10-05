40b2d7d300d9b3ca888d36e84395f6d144d4073f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2025-10-05 01:50:19 -0400

    add test data

---
+++ src/testdata/items.ts
@@ -0,0 +1,124 @@
+import { Character } from "../types";
+
+export const itemData: Character[] = [
+  {
+    name: 'Chasca',
+    country: 'Natlan',
+    element: 'anemo',
+    weapon: 'bow',
+    quality: 5,
+    img: 'chasca.png'
+  },
+  {
+    name: 'Mavuika',
+    country: 'Natlan',
+    element: 'pyro',
+    weapon: 'claymore',
+    quality: 5,
+    img: 'mavuika.png'
+  },
+  {
+    name: 'Citlali',
+    country: 'Natlan',
+    element: 'cryo',
+    weapon: 'catalyst',
+    quality: 5,
+    img: 'citlali.png'
+  },
+  {
+    name: 'Lan Yan',
+    country: 'Liyue',
+    element: 'anemo',
+    weapon: 'catalyst',
+    quality: 4,
+    img: 'lanyan.png'
+  },
+  {
+    name: 'Yumemizuki Mizuki',
+    country: 'Inazuma',
+    element: 'anemo',
+    weapon: 'catalyst',
+    quality: 5,
+    img: 'yumemizuki-mizuki.png'
+  },
+  {
+    name: 'Varesa',
+    country: 'Natlan',
+    element: 'electro',
+    weapon: 'catalyst',
+    quality: 5,
+    img: 'varesa.png'
+  },
+  {
+    name: 'Iansan',
+    country: 'Natlan',
+    element: 'electro',
+    weapon: 'polearm',
+    quality: 4,
+    img: 'iansan.png'
+  },
+  {
+    name: 'Escoffier',
+    country: 'Fontaine',
+    element: 'cryo',
+    weapon: 'polearm',
+    quality: 5,
+    img: 'escoffier.png'
+  },
+  {
+    name: 'Ifa',
+    country: 'Natlan',
+    element: 'anemo',
+    weapon: 'catalyst',
+    quality: 4,
+    img: 'ifa.png'
+  },
+  {
+    name: 'Skirk',
+    country: 'Other',
+    element: 'cryo',
+    weapon: 'sword',
+    quality: 5,
+    img: 'skirk.png'
+  },
+  {
+    name: 'Dahlia',
+    country: 'Mondstadt',
+    element: 'hydro',
+    weapon: 'sword',
+    quality: 4,
+    img: 'dahlia.png'
+  },
+  {
+    name: 'Ineffa',
+    country: 'Nod-Krai',
+    element: 'electro',
+    weapon: 'polearm',
+    quality: 5,
+    img: 'ineffa.png'
+  },
+  {
+    name: 'Lauma',
+    country: 'Nod-Krai',
+    element: 'dendro',
+    weapon: 'catalyst',
+    quality: 5,
+    img: 'lauma.png'
+  },
+  {
+    name: 'Aino',
+    country: 'Nod-Krai',
+    element: 'hydro',
+    weapon: 'claymore',
+    quality: 4,
+    img: 'aino.png'
+  },
+  {
+    name: 'Flins',
+    country: 'Nod-Krai',
+    element: 'electro',
+    weapon: 'polearm',
+    quality: 5,
+    img: 'flins.png'
+  },
+]

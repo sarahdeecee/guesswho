5dfff5ece677c3b58b360b65a967e54d13c85ba2
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2025-10-05 01:51:18 -0400

    add Board to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { TopNav } from './TopNav';
+import { Board } from './Board';
+import { itemData } from './testdata/items';
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <Board cols={5} itemData={itemData} />
     </div>
   );
 }

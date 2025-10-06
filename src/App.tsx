6d7f2cda60307a96f110194e19d29a63017738f0
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2025-10-05 23:50:27 -0400

    add state and add temporary piece select function

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { TopNav } from './components/TopNav';
 import { Board } from './components/Board';
 import { itemData } from './testdata/items';
+import { useState } from 'react';
 
 function App() {
+  const [selected, setSelected] = useState(itemData);
   return (
     <div className="App">
       <TopNav />
-      <Board cols={5} itemData={itemData} />
+      <Board cols={5} items={itemData} selected={selected} setSelected={setSelected} />
     </div>
   );
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,19 @@
 import { Box, ImageList, ImageListItem } from '@mui/material';
-import { Character } from '../types';
+import { Character, CharacterPiece } from '../types';
 
 export function Board(props: any) {
-  const {cols, itemData} = props;
+  const {cols, items, selected, setSelected} = props;
   const width: number = cols * 164;
+  const select = (): void => console.log('select')
+  // const select: void = setSelected([
+  //   ...selected
+  // ])
+
   return (
     <Box display='flex' width='100vw' sx={{justifyContent: 'center'}} >
       <ImageList gap={10} cols={cols} rowHeight={164} sx={{maxWidth: width}}>
-        {itemData.map((item: Character) => (
-          <ImageListItem key={item.img}>
+        {items.map((item: CharacterPiece) => (
+          <ImageListItem key={item.img} onClick={select}>
             <img
               src={`/images/characters/${item.img}`}
               alt={item.name}
